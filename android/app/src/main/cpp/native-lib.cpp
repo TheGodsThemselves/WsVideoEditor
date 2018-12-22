6120830fbc39d50c4a7d0204c7c1e509a9e0f363
whenSunset <a1018998632@gmail.com>
2018-12-22 16:20:54 +0800

    init repo

---
+++ android/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_whensunset_mytiktokvideoeditor_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
